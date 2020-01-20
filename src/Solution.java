3c26f57d4669f706279908516ada839cc7afd7ae
LYY-y <1312152518@qq.com>
2020-01-20 10:15:10 +0800

    leetcode200120

---
+++ src/Solution.java
@@ -0,0 +1,59 @@
+import java.util.HashMap;
+import java.util.Map;
+
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+//你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+//
+//        来源：力扣（LeetCode）
+//        链接：https://leetcode-cn.com/problems/two-sum
+//        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i=0, j=0;
+
+        for(i=0; i < nums.length; i++) {
+            for (j = i + 1; j < nums.length; j++) {
+                 if (target == nums[i] + nums[j]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{i,j};
+    }
+//以下来自leetcode官方解题
+    public int[] twoSumA(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public int[] twoSumB(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+
+
+
+    public static void main(String[] args){
+        Solution s = new Solution();
+        s.twoSumB(new int[]{2,5,5,11},10);
+    }
+
+}
