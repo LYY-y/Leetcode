bff9318ce65685b13d12e097d9075ea074000fee
LYY-y <1312152518@qq.com>
2020-01-21 11:54:01 +0800

    TwoSumByPy finish200121

---
+++ src/TwoSum.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    #用字典实现HashMap，成功
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict={}
+        for k, v in enumerate(nums):
+            dict[v]=k
+
+        for k,v in enumerate(nums):
+            print(k,v)
+            complement = target-v
+            print(complement)
+            print(dict.keys())
+            print(dict.get(complement))
+            if complement in dict.keys() and dict.get(complement) != k:
+                print (v)
+                print (dict.get(complement))
+                print ([k,dict.get(complement)])
+                return [k,dict.get(complement)]
+            
+        
+x=Solution()
+x.twoSum([3,2,4], 6)
+
+"""
+def twoSum(nums, target):
+    hashmap={}
+    for ind,num in enumerate(nums):
+        hashmap[num] = ind
+    for i,num in enumerate(nums):
+        j = hashmap.get(target - num)
+        if j is not None and i!=j:
+            return [i,j]
+
+
+
+作者：lao-la-rou-yue-jiao-yue-xiang
+链接：https://leetcode-cn.com/problems/two-sum/solution/xiao-bai-pythonji-chong-jie-fa-by-lao-la-rou-yue-j/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+"""
