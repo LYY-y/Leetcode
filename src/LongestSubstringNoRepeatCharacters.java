caa5fd328e34d85e9a0636bc7b885d7c85ac800c
LYY-y <1312152518@qq.com>
2020-01-24 18:15:15 +0800

    updateLongestSubStringNoRepeat

--- src/LongestSubstringNoRepeatCharacters.java
+++ src/LongestSubstringNoRepeatCharacters.java
@@ -3,17 +3,46 @@ import java.util.Map;
 
 public class LongestSubstringNoRepeatCharacters {
     public int lengthOfLongestSubstring(String s) {
-        int count=0;
-        Map<Integer,Integer> map=new HashMap<Integer, Integer>();
-        for (int i=0; i<s.length(); i++){
-            int single=(int)s.charAt(i);
-            map.put(i,single);
-            for (int begin=0; begin<=i; begin++){
-                int sum=0;
-                if (map.get(begin)==map.get())
+        if (s.length()==1){
+            return 1;
+        }else {
+            int count=0;
+            Map<Integer,String> map=new HashMap<Integer, String>();
+            //从左到右将不重复的字符编码存进HashMap
+            for (int j=0; j<s.length(); j++) {
+                for (int i = j; i < s.length(); i++) {
+                    if (map.containsValue(s.substring(i,i+1))) {//  检查map是否已有该值
+                        if (count < map.size()) {
+                            count = map.size();
+                        }
+                        map.clear();
+                        break;
+                    } else {
+                        map.put(i, s.substring(i,i+1));
+                    }
+
+                }
             }
-
+            return count;
         }
+    }
 
+    public int lengthOfLongestSubstring2(String s) {
+        int count=0;
+        Map<Character,Integer> map=new HashMap<Character,Integer>();
+        for (int i=0; i<s.length(); i++){
+            if (map.containsKey(s.charAt(i))){
+                
+                map.remove(s.charAt(i));
+            }
+            map.put(s.charAt(i),i);
+            count=Math.max(map.size(),count);
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        LongestSubstringNoRepeatCharacters test=new LongestSubstringNoRepeatCharacters();
+        System.out.println(test.lengthOfLongestSubstring(" "));
     }
 }
