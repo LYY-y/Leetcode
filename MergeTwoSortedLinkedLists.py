beb5f96273219586bdb5848f09b16694dca7eba2
LYY-y <1312152518@qq.com>
2020-02-15 16:03:35 +0800

    合并两个有序链表200215

--- MergeTwoSortedLinkedLists.py
+++ MergeTwoSortedLinkedLists.py
@@ -15,7 +15,14 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
+'''
+解法一：
+1、若其中一条链为空，则返回另一条
+2、将头部较小的定为L1，用head1指L1头部，rear2记录L2的next位置
+3、以L1为基准，若L1的next>=L2，则将L2插入L1前，两者指针均后移一位；
+   否则只移动L1指针，直至L1结束
+4、若L2此时不为空，则将L2接到L1后面
+'''
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         if(l1==None):
@@ -41,6 +48,32 @@ class Solution:
         if l2!=None:
             l1.next=l2
         return head1
+'''
+解法二：
+1、新建一个链表
+2、在L1、L2链表中寻找较小值插入新链表中
+'''
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if(l1==None):
+            return l2
+        elif(l2==None):
+            return l1
+        head=ListNode(None)
+        p=head
+        while l1!=None and l2!=None:
+            if l1.val<=l2.val:
+                p.next=l1
+                l1=l1.next
+            else:
+                p.next=l2
+                l2=l2.next
+            p=p.next
+        if l2!=None:
+            p.next=l2
+        elif l1!=None:
+            p.next=l1
+        return head.next
 '''
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
